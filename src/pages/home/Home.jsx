ad5407c8bf14fb1754db22e747b552a231b953ca
Faisal Khan <faisalkh01122@gmail.com>
2024-11-01 23:52:05 -0700

    To set Diffrent color and padding etc

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import Slider from './Slider'; // Ensure the import path is correct
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Slider from "./Slider"; // Ensure the import path is correct
+import Kg from "./kg";
 
 const Home = () => {
   const { t } = useTranslation(); // Hook to access the translation function
 
   return (
-    <div className="">
-      <Slider />
-    </div>
+    <>
+      <div className="w-full">
+        <Slider />
+        <Kg />
+      </div>
+    </>
+    // <div className="w-full">
+
+    // </div>
   );
 };
 
--- src/pages/home/Slider.jsx
+++ src/pages/home/Slider.jsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import Slider from 'react-slick';
-import { useTranslation } from 'react-i18next'; // Importing the translation hook
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
+import { useTranslation } from "react-i18next"; // Importing the translation hook
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const sliderItems = [
   {
-    imgSrc: "https://www.machinelines.com/wp-content/themes/lines/img/img_key05.jpg",
+    imgSrc:
+      "https://www.machinelines.com/wp-content/themes/lines/img/img_key05.jpg",
     catchKey: "high_quality_machinery",
     leadKey: "selling_machinery",
     btnLink: "/",
     btnTextKey: "list_of_second_hand_machinery",
   },
   {
-    imgSrc: "https://www.machinelines.com/wp-content/themes/lines/img/img_key01.jpg",
+    imgSrc:
+      "https://www.machinelines.com/wp-content/themes/lines/img/img_key01.jpg",
     catchKey: "high_quality_machinery",
     leadKey: "selling_machinery",
     btnLink: "/",
     btnTextKey: "list_of_second_hand_machinery",
   },
   {
-    imgSrc: "https://www.machinelines.com/wp-content/themes/lines/img/img_key04.jpg",
+    imgSrc:
+      "https://www.machinelines.com/wp-content/themes/lines/img/img_key04.jpg",
     catchKey: "exclusive_information",
     leadKey: "membership_access",
     btnLink: "/",
     btnTextKey: "get_membership",
   },
   {
-    imgSrc: "https://www.machinelines.com/wp-content/themes/lines/img/img_key03.jpg",
+    imgSrc:
+      "https://www.machinelines.com/wp-content/themes/lines/img/img_key03.jpg",
     catchKey: "high_quality_machinery",
     leadKey: "selling_machinery",
     btnLink: "/",
     btnTextKey: "list_of_second_hand_machinery",
   },
   {
-    imgSrc: "https://www.machinelines.com/wp-content/themes/lines/img/img_key05.jpg",
+    imgSrc:
+      "https://www.machinelines.com/wp-content/themes/lines/img/img_key05.jpg",
     catchKey: "high_quality_machinery",
     leadKey: "selling_machinery",
     btnLink: "/",
@@ -57,31 +61,40 @@ const SliderComponent = () => {
   };
 
   return (
-    <div className="slider-container w-full overflow-hidden">
-      <Slider {...settings}>
-        {sliderItems.map((item, index) => (
-          <div key={index} className="slide-item relative w-full h-screen">
-            <div
-              className="bg-cover bg-center h-full w-full"
-              style={{ backgroundImage: `url(${item.imgSrc})` }}
-            >
-              <div className="flex flex-col justify-center items-start h-full p-8 text-white">
-                <p className="catch text-3xl font-bold" style={{fontSize:"4rem", lineHeight:"2"}}>{t(item.catchKey)}</p>
-                <p className="lead text-xl mt-2" style={{fontSize:"2rem"}}>{t(item.leadKey)}</p>
-                <p className="btn01 mt-4">
-                  <a
-                    href={item.btnLink}
-                    className="inline-block px-6 py-2 text-white bg-green-600 hover:bg-green-500 rounded-lg transition duration-300"
+    <>
+      <div className="slider-container w-full  overflow-hidden">
+        <Slider {...settings}>
+          {sliderItems.map((item, index) => (
+            <div key={index} className="slide-item relative w-full h-screen">
+              <div
+                className="bg-cover bg-center h-full w-full"
+                style={{ backgroundImage: `url(${item.imgSrc})` }}
+              >
+                <div className="flex flex-col justify-center items-start h-full p-8 text-white">
+                  <p
+                    className="catch text-3xl font-bold"
+                    style={{ fontSize: "4rem", lineHeight: "2" }}
                   >
-                    {t(item.btnTextKey)}
-                  </a>
-                </p>
+                    {t(item.catchKey)}
+                  </p>
+                  <p className="lead text-xl mt-2" style={{ fontSize: "2rem" }}>
+                    {t(item.leadKey)}
+                  </p>
+                  <p className="btn01 mt-4">
+                    <a
+                      href={item.btnLink}
+                      className="inline-block px-6 py-2 text-white bg-green-600 hover:bg-green-500 rounded-lg transition duration-300"
+                    >
+                      {t(item.btnTextKey)}
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Slider>
-    </div>
+          ))}
+        </Slider>
+      </div>
+    </>
   );
 };
 
