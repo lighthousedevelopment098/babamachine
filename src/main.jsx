4e39890dc13bd22fd4dedc9f714e3166d78d576f
Faisal Khan <faisalkh01122@gmail.com>
2024-11-02 00:07:56 -0700

    main changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/globals.css';
-import './config/i18n';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/globals.css";
+import "./config/i18n";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
       <App />
